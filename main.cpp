64a4b89a69d36ea78d11294311ed78e6fb026e16
rawwanntarekk <11410120211039@stud.cu.edu.eg>
2022-11-11 21:27:18 +0200

    Overloading >> operator

--- main.cpp
+++ main.cpp
@@ -1,149 +1,255 @@
-// FCAI – Object-Oriented Programming 1 – 2022 - Assignment 1
-// Program Name: BigDecimalInt
-// Last Modification Date: 13/10/2022
-// Author1 and ID and Group: Basmala Mohamed Sayed Gad  ID: 20210090 (a, b, c)
-// Author2 and ID and Group: Aya Ali Hassan  ID: 20210083 (d, e)
-// Author3 and ID and Group: Mohamed Ashraf Fahim  ID: 20210329 (f, i, j , k)
-/*
-description: In this problem we developed a new C++ type (class).
-that can hold unlimited decimal integer values and performs arithmetic operations on them.
-such as: +, -, <, and >.
- */
-
+#include <iostream>
+#include "BigDecimalIntClass.h"
 #include "BigDecimalIntClass.cpp"
+#include <string>
+using namespace std;
 
-int main() {
-/*    string n1 = "-34", n2 = "100", n3 = "-500", n4 = "7";
+string push_back(BigDecimalInt a ,const string& zero){
+    string Result = a.getNumber() + zero ;
+    if(a.sign() == 0){Result = '-' + Result ;}
+    return Result ;
+}
 
-    BigDecimalInt input(n1), input3(n2);
-    BigDecimalInt e = input + input3;
-    BigDecimalInt d = input - input3;
-    cout << "---------TEST ONE---------" << "\n";
-    cout << e;
-    cout << d;
-    cout << (input < input3) << "\n";
-    cout << (input > input3) << "\n";
-    cout << input.Sign() << "\n";
-    cout << input3.size() << "\n";
+class BigReal {
+    private:
+        BigDecimalInt integer ;
+        BigDecimalInt fraction ;
+        BigDecimalInt sum ;
+        string result ;
+    public:
 
-    BigDecimalInt in(n1), in2(n3);
-    BigDecimalInt a = in + in2;
-    BigDecimalInt b = in - in2;
-    cout << "---------TEST TWO---------" << "\n";
-    cout << a;
-    cout << b;
-    cout << (in < in2) << "\n";
-    cout << (in > in2) << "\n";
-    cout << in2.Sign() << "\n";
-    cout << in.size() << "\n";
+        BigReal(){
+            integer.setNumber("0") ;
+            fraction.setNumber("0");
+            sum.setNumber("0");
+            result = "0" ;
+        }
 
-    BigDecimalInt input6(n2), input7(n3);
-    BigDecimalInt h = input6 + input7;
-    BigDecimalInt f = input6 - input7;
-    cout << "---------TEST THREE---------" << "\n";
-    cout << h;
-    cout << f;
-    cout << (input6 < input7) << "\n";
-    cout << (input6 > input7) << "\n";
-    cout << input6.Sign() << "\n";
-    cout << input7.size() << "\n";
+        explicit BigReal(const string& number){
 
-    BigDecimalInt in1(n4), in3(n2);
-    BigDecimalInt k = in1 + in3;
-    BigDecimalInt l = in1 - in3;
-    cout << "---------TEST FOUR---------" << "\n";
-    cout << k;
-    cout << l;
-    cout << (in1 < in3) << "\n";
-    cout << (in1 > in3) << "\n";
-    cout << in1.Sign() << "\n";
-    cout << in3.size() << "\n";
+            string I , F ;
+            int point_found = 0 ;
+            for(char i : number){
+                if(i == '.'){
+                    point_found += 1 ;
+                }
+                if(point_found == 0){
+                    I.push_back(i);
+                }
+                else if(point_found == 1 && i != '.' ){
+                    F.push_back(i);
+                }
+                else if(point_found > 1){
+                    cout<< "invalid" << endl ;
+                    exit(1);
+                }
+            }
 
-    BigDecimalInt test0(n3), test11(n3);
-    BigDecimalInt c = test0 + test11;
-    BigDecimalInt g = test0 - test11;
-    cout << "---------TEST FIVE---------" << "\n";
-    cout << c;
-    cout << g;
-    cout << (test0 < test11) << "\n";
-    cout << (test0 > test11) << "\n";
-    cout << test0.Sign() << "\n";
-    cout << test11.size() << "\n";
+            if(I.empty()){
+                integer.setNumber("0") ;
+                result = "0" ;
+            }
+            else{
+                integer.setNumber(I) ;
+                result = I ;
+            }
 
-    BigDecimalInt test(n3), test2(n2);
-    BigDecimalInt j = test + test2;
-    BigDecimalInt i = test - test2;
-    cout << "---------TEST SIX---------" << "\n";
-    cout << j;
-    cout << i;
-    cout << (test < test2) << "\n";
-    cout << (test > test2) << "\n";
-    cout << test.Sign() << "\n";
-    cout << test2.size() << "\n";
+            if(F.empty()){
+                fraction.setNumber("0") ;
+                result += ".0" ;
+            }
+            else{
+                fraction.setNumber(F) ;
+                result += "." + F ;
+            }
+            sum = integer ;
+            sum.setNumber(push_back(sum , fraction.getNumber()));
+        }
 
-    BigDecimalInt test3(n2), test4(n1);
-    BigDecimalInt o = test3 + test4;
-    BigDecimalInt p = test3 - test4;
-    cout << "---------TEST SEVEN---------" << "\n";
-    cout << o;
-    cout << p;
-    cout << (test3 < test4) << "\n";
-    cout << (test3 > test4) << "\n";
-    cout << test3.Sign() << "\n";
-    cout << test4.size() << "\n";
 
-    BigDecimalInt test5(n3), test6(n4);
-    BigDecimalInt q = test5 + test6;
-    BigDecimalInt w = test5 - test6;
-    cout << "---------TEST EIGHT---------" << "\n";
-    cout << q;
-    cout << w;
-    cout << (test5 < test6) << "\n";
-    cout << (test5 > test6) << "\n";
-    cout << test5.Sign() << "\n";
-    cout << test6.size() << "\n";
+        static void add_zeros(BigReal& a ,BigReal& b){
+            int result = (a.fraction.size() - b.fraction.size()) ;
+            for(int i = 0 ; i < abs(result) ; i++){
+                if(result > 0){
+                    b.sum.setNumber(push_back(b.sum,"0"));
+                }
+                else if(result < 0){
+                    a.sum.setNumber(push_back(a.sum,"0"));
+                }
+                else{
+                    break;
+                }
+            }
+        }
 
-    BigDecimalInt test7(n3), test8(n1);
-    BigDecimalInt m = test7 + test8;
-    BigDecimalInt n = test7 - test8;
-    cout << "---------TEST NINE---------" << "\n";
-    cout << m;
-    cout << n;
-    cout << (test7 < test8) << "\n";
-    cout << (test7 > test8) << "\n";
-    cout << test7.Sign() << "\n";
-    cout << test8.size() << "\n";
+        friend ostream& operator<<(ostream& out , const BigReal& other){
+            out<<other.result ;
+            return out ;
+        }
 
-    BigDecimalInt test9(n2), test10(n4);
-    BigDecimalInt x = test9 + test10;
-    BigDecimalInt y = test9 - test10;
-    cout << "---------TEST TEN---------" << "\n";
-    cout << x;
-    cout << y;
-    cout << (test9 < test10) << "\n";
-    cout << (test9 > test10) << "\n";
-    cout << test9.Sign() << "\n";
-    cout << test10.size() << "\n";*/
-    // string s1,s2;
-    // while(cin>>s1>>s2){
-    //     BigDecimalInt a(s1),b(s2);
-    //     cout<<a+b<<endl;
-    //     cout<<a-b<<endl;
-    //     cout<<b-a<<endl;
-    //     cout << (a < b) << "\n";
-    //     cout << (a > b) << "\n";
-    //     cout << a.sign() << "\n";
-    //     cout << b.size() << "\n";
-    //     cout << a.getNumber() << "\n";
-    //     a.setNumber("100");
-    //     cout << a.getNumber() << "\n";
-    // }
-    
-    BigReal a("1234.45") , b("2313.6");
-    cout<< a+b ;
-    
-    return 0;
+
+
+        BigReal operator+(BigReal other){
+            BigReal Return ;
+            Return.integer = integer + other.integer ;
+            Return.fraction = fraction + other.fraction ;
+            add_zeros(*this ,other) ;
+            Return.sum = (sum + other.sum) ;
+            string sign ;
+            if(Return.sum.sign() == 1){sign = "" ;}
+            else if (Return.sum.sign() == 0){sign = '-';}
+            string test = (Return.integer.getNumber() + '.' + Return.fraction.getNumber()) , Result = Return.sum.getNumber() ;
+            for(int i = 0 ; i < Result.length() ; i++){
+                if(Result[i] != test[i] && (abs((Result[i]- '0')-(test[i]- '0')) == 1)){
+                    Result.insert(i+1,".");
+                    break;
+                }
+                else if(Result[i] != test[i]){
+                    Result.insert(i,".");
+                    break;
+                }
+            }
+            if(Result[0] == '.'){
+                Result = '0' + Result ;
+            }
+            Return.result = (sign + Result) ;
+            return Return ;
+        }
+
+        BigReal operator-(BigReal& other){
+            BigReal Return ;
+            Return.integer = integer - other.integer ;
+            Return.fraction = fraction - other.fraction ;
+            add_zeros(*this ,other) ;
+            Return.sum = (sum - other.sum) ;
+            string sign ;
+            if(Return.sum.sign() == 1){sign = "" ;}
+            else if (Return.sum.sign() == 0){sign = '-';}
+            string test = (Return.integer.getNumber() + '.' + Return.fraction.getNumber()) , Result = Return.sum.getNumber() ;
+            for(int i = 0 ; i < Result.length() ; i++){
+                if(Result[i] != test[i] && (abs((Result[i]- '0')-(test[i]- '0')) == 1)){
+                    Result.insert(i+1,".");
+                    break;
+                }
+                else if(Result[i] != test[i]){
+                    Result.insert(i,".");
+                    break;
+                }
+            }
+            if(Result[0] == '.'){
+                Result = '0' + Result ;
+            }
+            Return.result = (sign + Result) ;
+            return Return ;
+        }
+
+
+bool operator > ( BigReal& anotherReal) {
+
+            if (this->integer > anotherReal.integer) {
+                return true;
+            }
+            else if (this->integer == anotherReal.integer) {
+                if (this->fraction > anotherReal.fraction) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+    }
+    bool operator < ( BigReal &anotherReal) {
+
+            if (this->integer < anotherReal.integer) {
+                return true;
+            }
+            else if (this->integer == anotherReal.integer) {
+                if (this->fraction < anotherReal.fraction) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+    }
+    bool operator == ( BigReal &anotherReal){
+                if (sum.sign() == anotherReal.sum.sign() && integer == anotherReal.integer &&fraction == anotherReal.fraction&& fraction.size() == anotherReal.fraction.size()  ){
+                    return true ;
+                }
+                else {
+                    return false;
+                }
+        }
+
+
+
+        int Sign(){
+            if (sum.sign() == 1){
+                return 1 ;
+            }
+            else{
+                return -1;
+            }
+        }
+
+        int Size (){
+            return sum.size() ;
+        }
+
+       BigReal &operator=(BigReal &anotherReal);
+
+        friend istream& operator>>(istream& in, BigReal& other) {
+        cout << "Enter a number: ";
+        string number ;
+        in >> number;
+        BigReal temp(number);
+        other = temp;
+        return in;
+
+    }
+
+};
+
+BigReal &BigReal::operator=(BigReal &anotherReal) {
+    integer = anotherReal.integer;
+    fraction = anotherReal.fraction;
+    sum = anotherReal.sum ;
+    result = anotherReal.result ;
+    return *this;
 }
 
 
+int main(){
+    //BigDecimalInt a("232") , b("5796") ;
+    BigReal a("-232.9249999999999999") , b("5796.399"),c ;
+    cout << a <<endl;
+    cout << a+b<<endl;
+    cout << a-b <<endl;
+    cout << a.operator==(b) <<endl;
+    cout << a.operator>(b) <<endl;
+    cout << a.operator<(b) <<endl;
+    cout << a.Size()<<endl;
+    cout << a.Sign()<<endl;
+    cout << "------------------------------------------------------------"<<endl;
+    cin >> c;
+    cout << c <<endl;
+    cout << c - b <<endl;  //wrong output
+    cout << c.operator>(b) <<endl;
+    cout << c.operator<(b) <<endl;
+    cout << c.operator==(b) <<endl;
+    cout << c.Size()<<endl; //wrong output
+    cout << c.Sign()<<endl;
+    cout << "------------------------------------------------------------"<<endl;
+    //5564.534 done
+    //5563.466 done
+    //-5564.534 done
+    //-5563.466 done
+
+}
 
