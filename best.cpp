18f807020de61107107958778bd00b6d7ffc4922
mohamedmahmoudelgendy <103971845+mohamedmahmoudelgendy@users.noreply.github.com>
2022-11-11 23:40:10 +0200

    delete & from underworld :)

--- best.cpp
+++ best.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include "BigDecimalIntClass.h"
 #include "BigDecimalIntClass.cpp"
 #include <string>
 using namespace std;
 
-string push_back(BigDecimalInt a ,string zero){
+string push_back(BigDecimalInt a ,const string zero){
     string Result = a.getNumber() + zero ;
     if(a.sign() == 0){Result = '-' + Result ;}
     return Result ;
@@ -14,7 +15,7 @@ class BigReal {
         BigDecimalInt integer ;
         BigDecimalInt fraction ;
         BigDecimalInt sum ;
-        string result ;
+        string result ;    
     public:
         
         BigReal(){
@@ -23,26 +24,26 @@ class BigReal {
             sum.setNumber("0");
             result = "0" ;
         }
-        
-        BigReal(string number){
+
+        explicit BigReal(const string number){
             string I , F ;
             int point_found = 0 ;
-            for(int i = 0 ; i < number.length() ; i++ ){
-                if(number[i] == '.'){
+            for(char i : number){
+                if(i == '.'){
                     point_found += 1 ;
                 }
                 if(point_found == 0){
-                    I.push_back(number[i]);
+                    I.push_back(i);
                 }
-                else if(point_found == 1 && number[i] != '.' ){
-                    F.push_back(number[i]);
+                else if(point_found == 1 && i != '.' ){
+                    F.push_back(i);
                 }
                 else if(point_found > 1){
                     cout<< "invalid" << endl ;
                     exit(1);
                 }
             }
-        
+
             if(I.empty()){
                 integer.setNumber("0") ;
                 result = "0" ;
@@ -64,8 +65,7 @@ class BigReal {
             sum.setNumber(push_back(sum , fraction.getNumber()));
         }
 
-
-        void add_zeros(BigReal& a ,BigReal& b){
+        static void add_zeros(BigReal& a ,BigReal& b){
             int result = (a.fraction.size() - b.fraction.size()) ;
             for(int i = 0 ; i < abs(result) ; i++){
                 if(result > 0){
@@ -80,7 +80,7 @@ class BigReal {
             }
         }
 
-        friend ostream& operator<<(ostream& out , BigReal other){
+        friend ostream& operator<<(ostream& out , const BigReal other){
             out<<other.result ;
             return out ;
         }
@@ -102,7 +102,7 @@ class BigReal {
                 }
                 else if(Result[i] != test[i]){
                     Result.insert(i,".");
-                    break;    
+                    break;
                 }
             }
             if(Result[0] == '.'){
@@ -112,7 +112,7 @@ class BigReal {
             return Return ;
         }
 
-        BigReal operator-(BigReal& other){
+        BigReal operator-(BigReal other){
             BigReal Return ;
             Return.integer = integer - other.integer ;
             Return.fraction = fraction - other.fraction ;
@@ -129,7 +129,7 @@ class BigReal {
                 }
                 else if(Result[i] != test[i]){
                     Result.insert(i,".");
-                    break;    
+                    break;
                 }
             }
             if(Result[0] == '.'){
@@ -137,65 +137,60 @@ class BigReal {
             }
             Return.result = (sign + Result) ;
             return Return ;
-        }  
+        }
 
 
-bool operator > ( BigReal& anotherReal) {
+        bool operator > ( BigReal& anotherReal) {
 
-            if (this->integer > anotherReal.integer) {
-                return true;
-            }
-            else if (this->integer == anotherReal.integer) {
-                if (this->fraction > anotherReal.fraction) {
+                if (this->integer > anotherReal.integer) {
                     return true;
                 }
-                else {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-    }
-    bool operator < ( BigReal &anotherReal) {
-
-            if (this->integer < anotherReal.integer) {
-                return true;
-            }
-            else if (this->integer == anotherReal.integer) {
-                if (this->fraction < anotherReal.fraction) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-    }
-    bool operator == ( BigReal &anotherReal){
-                if (sum.sign() == anotherReal.sum.sign() && integer == anotherReal.integer &&fraction == anotherReal.fraction&& fraction.size() == anotherReal.fraction.size()  ){
-                    return true ;
+                else if (this->integer == anotherReal.integer) {
+                    if (this->fraction > anotherReal.fraction) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
                 }
                 else {
                     return false;
                 }
         }
-    BigReal operator = (BigReal &anotherReal){
-                integer = anotherReal.integer;
-                fraction = anotherReal.fraction;
-                sum = anotherReal.sum ;
-                result = anotherReal.result ;
-                return *this;
+
+        bool operator < ( BigReal &anotherReal) {
+
+                if (this->integer < anotherReal.integer) {
+                    return true;
+                }
+                else if (this->integer == anotherReal.integer) {
+                    if (this->fraction < anotherReal.fraction) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+        }
+
+        bool operator == ( BigReal &anotherReal){
+            if (sum.sign() == anotherReal.sum.sign() && integer == anotherReal.integer &&fraction == anotherReal.fraction&& fraction.size() == anotherReal.fraction.size()  ){
+                return true ;
             }
+            else {
+                return false;
+            }
+        }
 
         int Sign(){
             if (sum.sign() == 1){
                 return 1 ;
             }
             else{
-                return -1;
+                return 0;
             }
         }
 
@@ -203,16 +198,52 @@ bool operator > ( BigReal& anotherReal) {
             return sum.size() ;
         }
 
+       BigReal& operator=(BigReal &anotherReal) {
+            integer = anotherReal.integer;
+            fraction = anotherReal.fraction;
+            sum = anotherReal.sum ;
+            result = anotherReal.result ;
+            return *this;
+        }
+
+        friend istream& operator>>(istream& in, BigReal& other) {
+            string number ;   
+            cout << "Enter a number: ";
+            in >> number;
+            BigReal temp(number);
+            other = temp ;
+            return in;
+        }
+
 };
 
 int main(){
     //BigDecimalInt a("232") , b("5796") ;
-    BigReal a("232.924") , b("5796.39") ;
-    cout << a-b ;
-
+    BigReal a("-232.924999") , b("5796.399") , c ;
+    cout << a <<endl;
+    cout << b << endl ;
+    cout << a+b<<endl;
+    cout << a-b <<endl;
+    cout << a.operator==(b) <<endl;
+    cout << a.operator>(b) <<endl;
+    cout << a.operator<(b) <<endl;
+    cout << a.Size()<<endl;
+    cout << a.Sign()<<endl;
+    cout << "------------------------------------------------------------"<<endl;
+    cin >> c;
+    cout << c <<endl;
+    cout << b << endl ;
+    cout << c + b << endl ;
+    cout << c - b <<endl;  //wrong output
+    cout << c.operator==(b) <<endl;
+    cout << c.operator>(b) <<endl;
+    cout << c.operator<(b) <<endl;
+    cout << c.Size()<<endl; //wrong output
+    cout << c.Sign()<<endl;
+    cout << "------------------------------------------------------------"<<endl;
     //5564.534 done
     //5563.466 done
-    //-5564.534 done 
-    //-5563.466 done 
-    
+    //-5564.534 done
+    //-5563.466 done
+
 }
